291b9d99fe1297704b77db27d0ac3bd2cac100c7
Marshall Thompson <marshall@cognitect.com>
2016-11-29 10:01:35 -0500

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject client-examples "0.1.0-SNAPSHOT"
+  :description "A collection of samples and tutorials for learning the Datomic Client API at a Clojure REPL."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [com.datomic/clj-client "0.8.606"]])
