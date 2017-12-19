e43566e722ee353d98a83973fbae25c8dba218b5
Marshall Thompson <marshall@cognitect.com>
2017-12-19 11:33:36 -0500

    typo

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.datomic/client-pro "0.8.14"])
+                 [com.datomic/client-pro "0.8.14"]])
